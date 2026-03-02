8a21fdb616a54b25100820c412085ba378144d5d
sarthakroutray <sarthak.routray2006@gmail.com>
2026-03-02 12:18:27 +0530

    Build Fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/postcss' // Added for Oneiros styling
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(), // Integrated Tailwind v4 engine
   ],
   build: {
     rollupOptions: {
